739295bc699631ee37034d139cbfba15ec969363
kebele <ykemalbeler@gmail.com>
2020-05-20 17:47:03 +0300

    ekleme

--- 1-vue-cli/src/App.vue
+++ 1-vue-cli/src/App.vue
@@ -2,4 +2,32 @@
   <div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor, odit in nisi distinctio obcaecati illum? Nostrum quaerat officia ab excepturi inventore repellat libero sed eius? Harum voluptatibus fuga alias!</p>
   </div>
+
+  <!-- bir vue componetinde 3 ana bölüm olur 
+  <template></template>
+  <script></script>
+  <style></style>
+
+  template mutlaka olmalıdır diğerleri olmayabilir yine de component çalışır.
+
+   -->
 </template>
+
+<script>
+export default {
+  // burası template içindekileri kontrol eder
+  //burası normal html sayfasındaki nw Vue;({...}) ya denk gelir
+  //data eskisi gibi bir object şeklinde tanımlanamaz, burada artık data(){} function dur. data içindeki verileri de return bir object döndürür, orada tanımlarız
+
+  data(){
+    return {
+
+    }
+  } 
+}
+</script>
+
+<style lang="">
+  
+</style>
+
