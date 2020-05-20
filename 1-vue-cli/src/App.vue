434e208720cb3940f4198ecab1babbb10a8e0c98
kebele <ykemalbeler@gmail.com>
2020-05-21 00:21:34 +0300

    ekleme

--- 1-vue-cli/src/App.vue
+++ 1-vue-cli/src/App.vue
@@ -17,11 +17,37 @@ problem olmaması gerekiyor başlangıçtan kalan diğer component olan HelloeWo
 şimdi ürün listeletelim,
 components içine Product.vue isimli bir component yapalım
 bunu buraya import edelim, <myComp/> a ihtiyacımız yok, comment e aldım
+Product.vue içine v-for="index in 5" :key="index" şeklinde kod ile 5 tane product compunu dizdik
+şimdi önceki çalışmalardan listeler içindeki listeyi alıp buradaki Product componenti içine koyup verileri dinamik hale getirelim, oradan productList [aldık ve] burada data ya koyduk (data nın cli kullanımında bir function olduğuna dikkat)
+şimdi v-for döngümüzü ona göre ayarlayalım, v-for ile productList ten dizilimi yaptık ancak burada bir hata verdi, bunu sebebi şua nda bunu bir function içinden aldığımız için key değerini index ten alamazsın dedi bu yüzden her bir ürüne unique bir değer vermeliyiz, listenin iiçne gidip her üürün için ayrı bir id değeri vereceğiz, v-for ile bir veri döndürüyorsak burada ayrı bir id ye ihtiyacımız var. Vue burada sıralarını tutar, eğer bir id verirsek eşşiz bir değer vue bunu tutar vermezsek yine çalışır ama ileide sıkıntı çıkarabilir, çünkü sıralama değişirse ortalık karışıri vue sıraları tutar eğer id yoksa
+şimdi sıra arraydeki bilgileri Product.vue içine yollamak, ayrıca bu array a birde description ekleyelim
+
+componentler arası veri iletişimi
+bunun bir kaç yolu var;
+1. props 
+bunun içiçn root comp içindeyken yani ver göndereceğimiz componenti kullandığımız yerde o componentin içinde yollamak; burada product ı product olarak yollamak istiyoruz
+:product="product"
+
+Product.vue ya gidip script içinde props : ["product"]
+şeklinde ekleyeceğiz
+
+bundna sonra artık bu bilgileri kullanabilir hae geliriz
+yani title için {{prosut.title}}
+{{product.price}} vb şeklinde
+
+bu yöntem parettan child a veri iletimi yöntemi
+
+şimdi alta bir buton koyalım
+
 
  -->
   <div class="container">
     <!-- <myComp/>   -->
-    <Product v-for="index in 5" :key="index" />
+    <!-- <Product v-for="index in 5" :key="index" /> -->
+    <Product
+      :product="product" 
+      v-for="product in productList" 
+      :key="product.id" />
   </div>
 </template>
 
@@ -36,6 +62,33 @@ export default {
     //  myComp : MainContainer,
     Product,
     //  bu gösterim aslında Product : Product tır, es6 da bu şekilde de tanımlanabilir
+  },
+  data() {
+    return {
+      productList: [
+        {
+          id: 1,
+          title: "macbook pro",
+          price: 100,
+          count: 0,
+          description : "açıklama 1"
+        },
+        {
+          id: 2,
+          title: "iphone",
+          price: 50,
+          count: 100,
+          description : "açıklama 2"
+        },
+        {
+          id: 3,
+          title: "klavye",
+          price: 10,
+          count: 1000,
+          description : "açıklama 3"
+        },
+      ],
+    };
   },
 };
 </script>
--- 1-vue-cli/src/components/Product.vue
+++ 1-vue-cli/src/components/Product.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="product">
-        <strong>ürün adı</strong>
-        <p>açıklama</p>
-        <strong>fiyat : 23,45 | adet : 12,3</strong>
-    </div>
+  <div class="product">
+    <strong>{{ product.title }}</strong>
+    <p>{{ product.description }}</p>
+    <strong>fiyat : {{ product.price }} | adet : {{ product.count }}</strong>
+    <br><br>
+    <button>sepete ekle</button>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  props: ["product"],
+};
 </script>
 
 <style>
-    .product {
-        border : 2px dashed #666;
-        padding : 5px 10px;
-        width : 300px;
-        margin-bottom : 5px
-    }
-    .product:hover{
-        border : 2px solid #666;
-        transition : all .5s
-    }
-
-
+.product {
+  border: 2px dashed #666;
+  padding: 5px 10px;
+  width: 300px;
+  margin-bottom: 5px;
+}
+.product:hover {
+  border: 2px solid #666;
+  transition: all 0.5s;
+}
 </style>
