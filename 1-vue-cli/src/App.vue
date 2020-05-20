d5ff5e014f123c9142e8e26e792a40791a71a72b
kebele <ykemalbeler@gmail.com>
2020-05-20 17:12:32 +0300

    cli ekleme

---
+++ 1-vue-cli/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolor, odit in nisi distinctio obcaecati illum? Nostrum quaerat officia ab excepturi inventore repellat libero sed eius? Harum voluptatibus fuga alias!</p>
+  </div>
+</template>
